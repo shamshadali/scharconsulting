f2c39dc6684126438bcbbab5f8cef1c89cfad0c9
Shamshad Ali <saifi.shamshad@gmail.com>
2024-10-14 18:00:39 +0200

    Updating contact.php

--- contact.php
+++ contact.php
@@ -3,34 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Schar Consulting - Contact Us</title>
+    <title>Schar Solutions - Contact Us</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
     <style>
         body {
             font-family: Arial, sans-serif;
             line-height: 1.6;
             margin: 0;
             padding: 0;
-            background-color: #f4f4f4;
         }
-        header {
-            background-color: #ffffff;
-            color: white;
-            padding: 1rem;
-        }
-
         .header-image {
             width: 100%;
-            height: 300px;
-            background-color: #f4f4f4;
-            display: flex;
+            height: 400px;
+            background-size: cover;
             justify-content: center;
             align-items: center;
-            overflow: hidden;
-        }
-        .header-image img {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
+            background: url('/assets/images/contact_us.jpg');
         }
         .main-div {
             padding: 2rem;
@@ -41,10 +29,11 @@
         }
         h1 {
             color: #35424a;
+            font-weight: bold;
         }
         form {
             display: grid;
-
+            gap: 1rem;
         }
         label {
             font-weight: bold;
@@ -56,7 +45,7 @@
             border-radius: 4px;
             box-sizing: border-box;
         }
-        button {
+        .button {
             background-color: #35424a;
             color: white;
             border: none;
@@ -64,7 +53,7 @@
             cursor: pointer;
             border-radius: 4px;
         }
-        button:hover {
+        .button:hover {
             background-color: #2c3840;
         }
         footer {
@@ -74,26 +63,13 @@
             padding: 1rem;
             margin-top: 2rem;
         }
-        @media (max-width: 768px) {
-            .header-image {
-                height: 200px;
-            }
-            main {
-                padding: 1rem;
-            }
-            p {
-                margin: 0px;
-            }
-        }
     </style>
 </head>
 <body>
     <?php include 'header.php' ?>
-
     <div class="header-image">
-        <img src="/assets/images/contact_us.jpg"  />
-    </div>
 
+    </div>
     <section style="display: flex;">
         <div class="px-2 col-span-2" style="flex: 1;  display: flex; justify-content: center; padding-top:80px;">
             <div>
@@ -110,27 +86,29 @@
             <div class="main-div col-span-2">
                 <h1>Contact Us</h1>
                 <p>Please fill out the form below to get in touch with us. We'll get back to you as soon as possible.</p>
-                <form action="contact_submitted.php" method="POST">
-                    <p>
-                        <label for="name">Name:</label>
-                        <input type="text" id="name" name="name" required>
-                    </p>
-                    <p>
-                        <label for="phone">Phone:</label>
-                        <input type="tel" id="phone" name="phone" required>
-                    </p>
-                    <p>
-                        <label for="email">Email:</label>
-                        <input type="email" id="email" name="email" required>
-                    </p>
-                    <p>
-                        <label for="message">Message:</label>
-                        <textarea id="message" name="message" rows="10" cols ="4" required></textarea>
-                    </p>
-                    <p>
-                        <input type="submit" value="Submit">
-                    </p>
-                </form>
+                <div style="margin-top:10px;">
+                    <form action="contact_submitted.php" method="POST">
+                        <p>
+                            <label for="name">Name:</label>
+                            <input type="text" id="name" name="name" required>
+                        </p>
+                        <p>
+                            <label for="phone">Phone:</label>
+                            <input type="tel" id="phone" name="phone" required>
+                        </p>
+                        <p>
+                            <label for="email">Email:</label>
+                            <input type="email" id="email" name="email" required>
+                        </p>
+                        <p>
+                            <label for="message">Message:</label>
+                            <textarea id="message" name="message" rows="10" cols ="4" required></textarea>
+                        </p>
+                        <p>
+                            <input type="submit" value="Submit" class="button">
+                        </p>
+                    </form>
+                </div>
             </div>
         </div>
         <p style="color: green;"><flash[:notice]></p>
